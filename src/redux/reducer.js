392ba91b30c3bddec6cad6e105034cfad727234d
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-11-12 15:02:28 +0200

    change if to switch statement

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,11 @@
 import { ADD_TO_CART } from "./constant";
 
 export const cartData = (data = [], action) => {
-  if (action.type === ADD_TO_CART) {
-    console.warn("ADD_TO_CART condition", action);
-    return data;
-  } else {
-    return "no action called";
-  }
+  switch(action.type) {
+    case ADD_TO_CART:
+        console.warn("ADD_TO_CART condition", action);
+        return 1 + 1
+    default:
+        return "no action called"
+    }
 };
